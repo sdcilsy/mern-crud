6ee544fcb8e32b859931a3c13dceafae63f2c431
arizalsandi <arizalsandi@gmail.com>
2022-08-14 22:05:30 +0700

    change userlist

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -35,7 +35,7 @@ const UserList = () => {
   return (
     <div className="columns mt-5 is-centered">
       <div className="column is-half">
-        <Banner title="Users List Staging" />
+        <Banner title="Users List Cilist" />
         <Link to={`add`} className="button is-info">
           Add New
         </Link>
