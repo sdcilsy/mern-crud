934727f1d369a2d3a82febf6e30a84f68b9cb2b7
Taufik <profesorgreen36@gmail.com>
2022-08-23 21:33:17 +0700

    edit be endpoint to inggress

--- frontend/src/components/EditUser.js
+++ frontend/src/components/EditUser.js
@@ -18,7 +18,7 @@ const EditUser = () => {
   const updateUser = async (e) => {
     e.preventDefault();
     try {
-      await axios.patch(`http://backend:5000/users/${id}`, {
+      await axios.patch(`http://backend/users/${id}`, {
         name,
         email,
         gender,
@@ -30,7 +30,7 @@ const EditUser = () => {
   };
 
   const getUserById = async () => {
-    const response = await axios.get(`http://backend:5000/users/${id}`);
+    const response = await axios.get(`http://backend/users/${id}`);
     setName(response.data.name);
     setEmail(response.data.email);
     setGender(response.data.gender);
